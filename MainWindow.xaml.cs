4b42a12d3cc837c63928fd086d38995f3f07eb71
julia.murria <julia.murria@iesdoctorbalmis.com>
2020-10-26 20:59:31 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Act4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int Caracteres { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextTextBlock()
+        {
+            for (int i = 0; i < textBox.Text.Length; i++)
+            {
+                if (textBox.Text[i] != ' ')
+                {
+                    Caracteres++;
+                }
+            }
+        }
+
+        private void ContadorLabel()
+        {
+            if (Caracteres == textBox.MaxLength)
+            {
+                contador.Foreground = Brushes.Red;
+            }
+            else
+            {
+                Content = $"{Caracteres}/{textBox.MaxLength}";
+            }
+        }
+    }
+}
